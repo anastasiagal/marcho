18b8f4948f89c0f7bea5f1ad98a9f35a44feee42
unknown <anastasya.galyamova@gmail.com>
2021-06-10 12:13:46 +0500

    added section product

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,12 @@ $(function(){
         arrows: false
     });
 
+    $(".star").rateYo({
+        rating: 3.6,
+        starWidth: "17px",
+        normalFill: "#ccccce",
+        ratedFill: "#ffc35b",
+        readOnly: true
+      });
+
 });
